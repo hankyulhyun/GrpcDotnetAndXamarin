79fef02083271d55e4272980658f54e90e045129
Hankyul <hyunhankyul@gmail.com>
2019-09-23 22:30:26 +0900

    first commit of grpc dotnet and xamarin

---
+++ GrpcMobileApp/GrpcMobileApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GrpcMobileApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.MainPageViewModel();
+        }
+    }
+}
---
+++ GrpcMobileApp/GrpcMobileApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text;
+
+namespace GrpcMobileApp.ViewModels
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string welcomeText;
+        public string WelcomeText
+        {
+            get { return this.welcomeText; }
+            set { welcomeText = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("WelcomeText")); }
+        }
+
+        public MainPageViewModel()
+        {
+            this.WelcomeText = "Welcome MVVM";
+
+            Channel channel = new Channel("10.0.2.2", 5002, ChannelCredentials.Insecure);
+            var client = new GrpcGreeter.Greeter.GreeterClient(channel);
+
+            var reply = client.SayHello(new GrpcGreeter.HelloRequest { Name = "Xamarin Client" });
+
+            this.WelcomeText = reply.Message;
+        }
+    }
+}
